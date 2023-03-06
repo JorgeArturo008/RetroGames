1bdb18a7dd09ff7ee6648627a0aec423b15aa3ef
Jorge Arturo <ecastrillorojas@gmail.com>
2023-03-05 23:52:00 -0600

    Inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+{ path: 'login', component: LoginComponent},
+{path: 'registrar-usuario', component: RegistrarUsuarioComponent},
+{path: 'dashboard', component: DashboardComponent},
+{path: '**', redirectTo: 'login', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegistrarUsuarioComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
